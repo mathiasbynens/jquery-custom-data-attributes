ec8f7ffd417a40cf8ff28335d378690f3f152944
Mathias Bynens <mathias@qiwi.be>
2010-02-08 22:09:14 +0100

    Initial commit

--- jquery.dataAttr.min.js
+++ jquery.dataAttr.min.js
@@ -1,6 +1,6 @@
 /*!
  * HTML5 custom data attributes plugin for jQuery
- * @link http://github.com/mathiasbynens/HTML5-dataAttr-jQuery-plugin
+ * @link http://github.com/mathiasbynens/HTML5-custom-data-attributes-plugin-for-jQuery
  * @description An easy setter/getter for HTML5 data-* attributes
  * @author Mathias Bynens <http://mathiasbynens.be/>
  */
