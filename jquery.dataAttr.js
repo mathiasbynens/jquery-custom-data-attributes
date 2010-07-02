e525efdae8b35fb3caa1ec04a7d920e78a6a03f4
Mathias Bynens <mathias@qiwi.be>
2010-07-02 13:42:09 +0200

    Improved compression/minification

--- jquery.dataAttr.js
+++ jquery.dataAttr.js
@@ -1,11 +1,11 @@
 /*!
- * HTML5 custom data attributes plugin for jQuery
+ * HTML5 custom data attributes plugin for jQuery v1.0
  * @link http://github.com/mathiasbynens/HTML5-custom-data-attributes-plugin-for-jQuery
  * @description An easy setter/getter for HTML5 data-* attributes
  * @author Mathias Bynens <http://mathiasbynens.be/>
  */
 ;(function($) {
  $.fn.dataAttr = function(attr, val) {
-  return val ? $(this).attr('data-' + attr, val) : $(this).attr('data-' + attr);
+  return val ? this.attr('data-' + attr, val) : this.attr('data-' + attr);
  };
 })(jQuery);
