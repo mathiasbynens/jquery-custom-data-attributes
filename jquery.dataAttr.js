24707249ee2d20020040277d4a2cd6bef78d35a1
Mathias Bynens <mathias@qiwi.be>
2010-08-28 14:20:47 +0200

    Small optimizations

--- jquery.dataAttr.js
+++ jquery.dataAttr.js
@@ -4,8 +4,6 @@
  * @description An easy setter/getter for HTML5 data-* attributes
  * @author Mathias Bynens <http://mathiasbynens.be/>
  */
-;(function($) {
- $.fn.dataAttr = function(attr, val) {
-  return val ? this.attr('data-' + attr, val) : this.attr('data-' + attr);
- };
-})(jQuery);
+jQuery.fn.dataAttr = function(attr, val) {
+ return val ? this.attr('data-' + attr, val) : this.attr('data-' + attr);
+};
